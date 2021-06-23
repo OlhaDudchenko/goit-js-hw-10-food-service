b04030df5fad84b5ce4b77a18d13775d6e4ddab9
OlhaDudchenko <dudchenko.olha92@gmail.com>
2021-06-23 23:47:42 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const input = document.querySelector('.theme-switch__toggle');
 const menu = document.querySelector('.js-menu');
 document.body.classList.add(Theme.LIGHT);
 input.addEventListener('change', inputOnChange);
- changeThemeLight();
+
 showLocalStorageValue();
 
 function inputOnChange(e) {
@@ -38,7 +38,6 @@ function showLocalStorageValue() {
         input.checked=false;
     }
 
-    // localStorage.removeItem('position');
      
 };
 
